9be60cf120f2828e6b3f53f9eba7c5d21a7a81de
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2019-09-10 23:21:06 +0530

    Create source.js

---
+++ source.js
@@ -0,0 +1,18 @@
+const changeLineNumber=(line,offset)=>{
+  let matches=line.match(/([jJ].* )([0-9]+)/)
+  return matches?matches[1]+(+matches[2]+offset):line;
+}
+
+const moveSourceTo=(lines,oldLineNumber,newLineNumber) => {
+  return lines.map((l)=>changeLineNumber(l,newLineNumber-oldLineNumber))
+}
+
+const foobar=()=>{
+  const prog = document.getElementById('sourcecode').value;
+  console.log(prog);
+  let lines = prog.split("\n");
+  let oldLine = document.getElementById("oldline").value;
+  let newLine = document.getElementById("newline").value;
+  const newProg = moveSourceTo(lines,+oldLine,+newLine)
+  document.getElementById('output').innerText = newProg.join("\n");
+}
